b6bbbc91765bfae779332c623ee3d223e8195597
dongfeiwww <dongfeiwww@cebdc5fb-6614-2877-7c2f-7b402270416d>
2010-12-28 21:56:09 +0000

    new files

---
+++ trunk/src/PalindromePhrases.java
@@ -0,0 +1,10 @@
+// http://www.topcoder.com/wiki/display/tc/SRM+439
+// http://forums.topcoder.com/?module=Thread&threadID=687656&start=0&mc=2#1306268
+// harder
+public class PalindromePhrases {
+	public long getAmount(String[] words) { 
+		long res = 0;
+		return res;
+		
+	}
+}
